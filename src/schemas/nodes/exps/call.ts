5c1cfc5dbcce1fbbbcdcce534d8887870587bd9b
adr <andres@duarterengifo.com>
2025-02-22 16:41:24 -0500

    call first try

--- src/schemas/nodes/exps/call.ts
+++ src/schemas/nodes/exps/call.ts
@@ -1,6 +1,5 @@
 import { Schema } from "effect";
 import { type Token, tokenSchema } from "src/schemas/token/unions/all";
-import type { INode } from "../interfaces/internal-node";
 import { type Exp, type ExpEncoded, expSchema } from "./union";
 
 export type CallExpEncoded = {
@@ -10,12 +9,17 @@ export type CallExpEncoded = {
 	readonly args: readonly ExpEncoded[];
 };
 
-export class CallExp
-	extends Schema.TaggedClass<CallExp>()("CallExp", {
-		token: tokenSchema,
-		fn: Schema.suspend((): Schema.Schema<Exp, ExpEncoded> => expSchema),
-		args: Schema.Array(
-			Schema.suspend((): Schema.Schema<Exp, ExpEncoded> => expSchema),
-		),
-	})
-	implements INode {}
+export type CallExp = {
+	readonly _tag: "CallExp";
+	readonly token: Token;
+	readonly fn: ExpEncoded;
+	readonly args: readonly ExpEncoded[];
+};
+
+export const CallExp = Schema.TaggedStruct("CallExp", {
+	token: tokenSchema,
+	fn: Schema.suspend((): Schema.Schema<Exp, ExpEncoded> => expSchema),
+	args: Schema.Array(
+		Schema.suspend((): Schema.Schema<Exp, ExpEncoded> => expSchema),
+	),
+});
