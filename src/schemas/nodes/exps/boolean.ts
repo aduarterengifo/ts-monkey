0e41b1558b87af46a53403c90ffa81847ad85db7
adr <andres@duarterengifo.com>
2025-02-22 16:39:37 -0500

    boolexp worked first try

--- src/schemas/nodes/exps/boolean.ts
+++ src/schemas/nodes/exps/boolean.ts
@@ -1,20 +1,19 @@
 import { Schema } from "effect";
 import { tokenSchema } from "src/schemas/token/unions/all";
-import type { INode } from "../interfaces/internal-node";
 
 export type BoolExpEncoded = Schema.Schema.Encoded<typeof BoolExp>;
 
-export class BoolExp
-	extends Schema.TaggedClass<BoolExp>()("BoolExp", {
-		token: tokenSchema,
-		value: Schema.Boolean,
-	})
-	implements INode {}
+export const BoolExp = Schema.TaggedStruct("BoolExp", {
+	token: tokenSchema,
+	value: Schema.Boolean,
+});
+
+export type BoolExp = typeof BoolExp.Type;
 
 export const BoolExpEq = Schema.equivalence(BoolExp);
 
 export const nativeToBoolExp = (bool: boolean) =>
-	new BoolExp({
+	BoolExp.make({
 		token: {
 			_tag: `${bool}`,
 			literal: `${bool}`,
