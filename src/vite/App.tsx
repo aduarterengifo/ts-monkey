15afeb3847a81961d1d50f3a214b83bfcaf67453
adr <andres@duarterengifo.com>
2025-02-22 21:26:37 -0500

    add examples to site

--- src/vite/App.tsx
+++ src/vite/App.tsx
@@ -20,57 +20,80 @@ function App() {
 		}
 	};
 
+	const handleEnter = async (newText: string) => {
+		console.log("enter");
+		const userInput = newText
+			.substring(newText.lastIndexOf(PROMPT) + PROMPT.length)
+			.trim();
+		if (userInput.toLowerCase() === "clear") {
+			setText((prevText) => `${PROMPT}`);
+			return;
+		}
+		const returnValue = await runAndInterpret(instructions + userInput);
+
+		setInstructions(
+			(prevInstructions) =>
+				`${prevInstructions}${newText.substring(newText.lastIndexOf(PROMPT) + PROMPT.length).trim()}\n`,
+		);
+		console.log("instructions", instructions);
+		const evaluated = Match.value(returnValue).pipe(
+			Match.tag("ErrorObj", (errorObj) => {
+				return errorObj.message;
+			}),
+			Match.orElse((r) => {
+				return objInspect(r.evaluation);
+			}),
+		);
+
+		setEvaluations((evals) => [...evals, evaluated]);
+
+		setText((prevText) => `${prevText}${evaluated}\n${PROMPT}`);
+	};
+
 	const handleChange = async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		const newText = e.target.value;
 		setText(newText);
 		if (newText.charAt(newText.length - 1) === "\n") {
-			console.log("enter");
-			const userInput = newText
-				.substring(newText.lastIndexOf(PROMPT) + PROMPT.length)
-				.trim();
-			if (userInput.toLowerCase() === "clear") {
-				setText((prevText) => `${PROMPT}`);
-				return;
-			}
-			const returnValue = await runAndInterpret(instructions + userInput);
-
-			setInstructions(
-				(prevInstructions) =>
-					`${prevInstructions}${newText.substring(newText.lastIndexOf(PROMPT) + PROMPT.length).trim()}\n`,
-			);
-			console.log("instructions", instructions);
-			const evaluated = Match.value(returnValue).pipe(
-				Match.tag("ErrorObj", (errorObj) => {
-					return errorObj.message;
-				}),
-				Match.orElse((r) => {
-					return objInspect(r.evaluation);
-				}),
-			);
-
-			setEvaluations((evals) => [...evals, evaluated]);
-
-			setText((prevText) => `${prevText}${evaluated}\n${PROMPT}`);
+			await handleEnter(newText);
 		}
 	};
 	return (
-		<div className="font-mono">
-			<div className="flex flex-col items-start pb-2 pl-2 w-[80ch]">
-				ts-monkey repl
+		<div className="grid place-items-center h-screen">
+			<div className="relative w-[40ch] lg:w-[80ch]">
+				<div className="flex flex-col items-start absolute -top-10 left-3">
+					ts-monkey repl
+				</div>
+				<Textarea
+					value={text}
+					onKeyDown={handleKeyDown}
+					onChange={handleChange}
+					className="resize-none h-80 w-full"
+				/>
+				<div className="flex flex-col items-start w-full absolute top-90 left-0 h-40 overflow-y-scroll gap-2">
+					{examples.map((str, i) => (
+						<button
+							type="button"
+							className="text-left pl-3 cursor-pointer"
+							key={`example-${i}`}
+							onClick={(e) => {
+								const txt = `${PROMPT} ${str}\n`;
+								setText((prevText) => txt);
+								handleEnter(txt);
+							}}
+						>
+							{str}
+						</button>
+					))}
+				</div>
 			</div>
-			<Textarea
-				value={text}
-				onKeyDown={handleKeyDown}
-				onChange={handleChange}
-				className="resize-none h-80 w-full"
-			/>
+			{/* 
 			<div className="flex flex-col items-start w-full">
 				{examples.map((str, i) => (
 					<div className="text-left" key={`example-${i}`}>
 						{str}
 					</div>
 				))}
-			</div>
+			</div> */}
 		</div>
 	);
 }
