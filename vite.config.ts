aa1332139317864f0abb4a2d6a22d34d175c6573
adr <andres@duarterengifo.com>
2025-02-22 21:00:02 -0500

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-import path from 'path'
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-    resolve: {
-    alias: {
-      'src': path.resolve(__dirname, './src'),
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			src: path.resolve(__dirname, "./src"),
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
