473cb894c12aa0bdc4240a22a30978b7eacfddc2
adr <andres@duarterengifo.com>
2025-02-23 12:20:44 -0500

    chain rule works for the most part, probably not all cases

--- src/services/object/builtins.ts
+++ src/services/object/builtins.ts
@@ -1,3 +1,4 @@
+import { FuncExp } from "@/schemas/nodes/exps/function";
 import { BuiltInObj } from "@/schemas/objs/built-in";
 import { ErrorObj } from "@/schemas/objs/error";
 import { FunctionObj } from "@/schemas/objs/function";
@@ -10,46 +11,73 @@ import { BlockStmt } from "src/schemas/nodes/stmts/block";
 import { ExpStmt } from "src/schemas/nodes/stmts/exp";
 import type { Environment } from "./environment";
 
-const diff2 = (...args: Obj[]) =>
-	Effect.gen(function* () {
-		// this is already evaluated hence Obj and not exps.
-		const {
-			params,
-			body: { token, statements },
-			env,
-		} = (yield* Schema.decodeUnknown(
-			Schema.Tuple(
-				Schema.Struct({
-					params: Schema.Tuple(IdentExp),
-					body: BlockStmt,
-					env: Schema.Unknown,
-				}),
-			),
-		)(args))[0];
-		// ASSUME THERE IS ONLY A SINGLE STATEMENT - FOR NOW
-		const expStmt = (yield* Schema.decodeUnknown(Schema.Tuple(ExpStmt))(
-			statements,
-		))[0];
+// Where does this fit.
+// it would be here where
 
-		const newBody = BlockStmt.make({
-			token,
-			statements: [
-				ExpStmt.make({
-					...expStmt,
-					expression: DiffExp.make({
-						token: {
-							_tag: "diff",
-							literal: "diff",
-						},
-						exp: expStmt.expression,
-						params,
-					}),
-				}),
-			],
-		});
+const diff = (...args: Obj[]) =>
+	Schema.decodeUnknown(
+		Schema.Tuple(
+			Schema.Struct({
+				params: Schema.Tuple(Schema.Union(IdentExp, FuncExp)), // of a single ident.
+				body: BlockStmt,
+				env: Schema.Unknown, // setting this to env blows everything up
+			}),
+		),
+	)(args).pipe(
+		Effect.flatMap(
+			([
+				{
+					params,
+					body: { token, statements },
+					env,
+				},
+			]) =>
+				Match.value(params[0]).pipe(
+					Match.tag("FuncExp", () =>
+						Effect.gen(function* () {
+							return yield* Effect.succeed(true);
+							// f(g(x)) = f'(g(x)) * g'(x)
+						}).pipe(Effect.tap((e) => Effect.logDebug("func exp", params))),
+					),
+					Match.tag("IdentExp", () =>
+						Effect.gen(function* () {
+							// suppose instead I
 
-		return FunctionObj.make({ params, body: newBody, env: env as Environment });
-	});
+							// ASSUME THERE IS ONLY A SINGLE STATEMENT - FOR NOW
+							const expStmt = (yield* Schema.decodeUnknown(
+								Schema.Tuple(ExpStmt),
+							)(statements))[0];
+
+							const newBody = BlockStmt.make({
+								token,
+								statements: [
+									ExpStmt.make({
+										...expStmt,
+										expression: DiffExp.make({
+											token: {
+												_tag: "diff",
+												literal: "diff",
+											},
+											exp: expStmt.expression,
+											params,
+										}),
+									}),
+								],
+							});
+
+							return FunctionObj.make({
+								params,
+								body: newBody,
+								env: env as Environment,
+							});
+						}).pipe(
+							// Effect.tap((e) => Effect.logDebug("identExp case", params, e)),
+						),
+					),
+					Match.exhaustive,
+				),
+		),
+	);
 
 export const builtins = {
 	len: BuiltInObj.make({ fn: "len" }),
@@ -82,7 +110,7 @@ export const builtInFnMap = {
 				),
 			);
 		}),
-	diff: diff2,
+	diff,
 } as const;
 
 const builtinKeys = Object.keys(builtins) as (keyof typeof builtins)[]; // hack
